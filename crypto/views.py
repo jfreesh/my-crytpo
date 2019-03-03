a89a908bbfb534a9e11eceffec29afa6b091f2be
Jared Freesh <jarbfree@gmail.com>
2019-03-02 20:12:37 -0600

    CH 22, Initial search for a currency and output to prices url

--- crypto/views.py
+++ crypto/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
 	import requests
 	import json
@@ -15,3 +16,18 @@ def home(request):
 	api = json.loads(api_request.content)
 
 	return render(request, 'home.html', {'api': api, 'price': price})
+
+def prices(request):
+	import requests
+	import json
+
+	if request.method == 'POST':
+		quote = request.POST['quote']
+		quote = quote.upper()
+		crypto_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=USD")
+		crypto = json.loads(crypto_request.content)	
+		return render(request, 'prices.html', {'quote':quote, 'crypto':crypto})
+	else:
+		return render(request, 'prices.html', {})
+
+
